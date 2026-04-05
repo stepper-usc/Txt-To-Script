e21a10c07ef9756b810ec3a3ddaf669b35ababb6
stepper-usc <stepper@usc.edu>
2026-04-05 02:53:18 +0000

    created basic class structure for program

---
+++ parser.h
@@ -0,0 +1,30 @@
+#ifndef PARSER_H
+#define PARSER_H
+#include <string>
+#include <istream>
+#include "book.h"
+
+class Parser 
+{
+    public:
+
+        Parser();
+        virtual ~Parser();
+
+        /*
+        * Parses lines from a istream, dividing sections of narration and dialog.
+        * Sves parsed data into a provided Book.
+
+        * @param[in] istr
+        *   input stream (typically a file) to be parsed
+
+        * @param[in] book
+        *   Book class where parsed information will be stored.
+        */
+        
+        virtual void segment_lines(std::istream& istr, Book& book);
+
+
+};
+
+#endif
